035fdff748fd87d6428d55d8eab07a4c0dc10005
Hesam N <AnonCODER@tutanota.com>
2022-04-28 19:27:10 +0430

    Income changes
    - IncreaseIncome model created + custom admin view
    - DecreaseIncome model created + custom admin view

--- RevenueCalcSys/income/admin.py
+++ RevenueCalcSys/income/admin.py
@@ -1,10 +1,18 @@
 from django.contrib import admin
-from income.models.TripIncome import TripIncome
+from income.models.TripIncome import TripIncome, IncreaseIncome, DecreaseIncome
 
 class TripIncomeAdmin(admin.ModelAdmin):
     list_display = ['courier', 'income', 'trip_date']
     search_fields = ['courier__full_name', 'trip_date']
     readonly_fields = ['trip_date']
-    
+
+
+class IncreaseDecreaseIncomeAdmin(admin.ModelAdmin):
+    list_display = ['courier', 'amount', 'date']
+    search_fields = ['courier__full_name', 'date']
+    readonly_fields = ['date']
+
 
 admin.site.register(TripIncome, TripIncomeAdmin)
+admin.site.register(IncreaseIncome, IncreaseDecreaseIncomeAdmin)
+admin.site.register(DecreaseIncome, IncreaseDecreaseIncomeAdmin)
