dfde087f69c6cccb6fb8d3004da339cdf91615b1
Hesam N <AnonCODER@tutanota.com>
2022-04-29 01:49:31 +0430

    SaveWeeklyIncome signal created
    - This signal saves and updates weekly incomes based on daily incomes

--- RevenueCalcSys/income/signals.py
+++ RevenueCalcSys/income/signals.py
@@ -1,6 +1,6 @@
 from django.dispatch import receiver
-from django.db.models.signals import pre_save, post_save
-from income.models.DWIncome import DailyIncome
+from django.db.models.signals import post_save
+from income.models.DWIncome import DailyIncome, WeeklyIncome
 from income.models.TripIncome import TripIncome
 
 @receiver(post_save, sender=TripIncome)
@@ -12,5 +12,22 @@ def SaveDailyIncome(sender, instance, *args, **kwargs):
         daily_income.income += instance.income   
         daily_income.save()
 
+    # If it doesn't exist
     except DailyIncome.DoesNotExist:
         DailyIncome.objects.create(courier=instance.courier, income=instance.income, date=instance.trip_date)
+
+
+@receiver(post_save, sender=DailyIncome)
+def SaveWeeklyIncome(sender, instance, *args, **kwargs):
+    '''This signal saves and updates weekly incomes based on daily incomes'''
+    # If it's saturday
+    if instance.date.isoweekday() == 6:
+        # If it's already exists
+        try:
+            weekly_income = WeeklyIncome.objects.get(courier=instance.courier, saturday=instance.date)
+            weekly_income.income += instance.income
+            weekly_income.save()
+        
+        # If it doesn't exist
+        except WeeklyIncome.DoesNotExist:
+            WeeklyIncome.objects.create(courier=instance.courier, income=instance.income, saturday=instance.date)
