b72ab30667c1a42a85b1e8e3b29ebb7f507617dc
Hesam N <AnonCODER@tutanota.com>
2022-04-29 00:39:53 +0430

    Daily and weekly income models created
    - Custom admin page

--- RevenueCalcSys/income/admin.py
+++ RevenueCalcSys/income/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from income.models.TripIncome import TripIncome, IncreaseIncome, DecreaseIncome
+from income.models.DWIncome import DailyIncome, WeeklyIncome
+
 
 class TripIncomeAdmin(admin.ModelAdmin):
     list_display = ['courier', 'income', 'trip_date']
@@ -13,6 +15,19 @@ class IncreaseDecreaseIncomeAdmin(admin.ModelAdmin):
     readonly_fields = ['date']
 
 
+class DailyIncomeAdmin(admin.ModelAdmin):
+    list_display = ['courier', 'amount', 'date']
+    search_fields = ['courier__full_name', 'date']
+    readonly_fields = ['date']
+
+
+class WeeklyIncomeAdmin(admin.ModelAdmin):
+    list_display = ['courier', 'saturday', 'income']
+    search_fields = ['courier__full_name', 'saturday']
+
+
 admin.site.register(TripIncome, TripIncomeAdmin)
 admin.site.register(IncreaseIncome, IncreaseDecreaseIncomeAdmin)
 admin.site.register(DecreaseIncome, IncreaseDecreaseIncomeAdmin)
+admin.site.register(WeeklyIncome, WeeklyIncomeAdmin)
+admin.site.register(DailyIncome, DailyIncomeAdmin)
---
+++ RevenueCalcSys/income/models/DWIncome.py
@@ -0,0 +1,13 @@
+from django.db import models
+from courier.models import Courier
+
+class DailyIncome(models.Model):
+    courier = models.ForeignKey(Courier, on_delete=models.CASCADE)
+    amount = models.PositiveIntegerField()
+    date = models.DateField(auto_now_add=True)
+
+
+class WeeklyIncome(models.Model):
+    courier = models.ForeignKey(Courier, on_delete=models.CASCADE)
+    saturday = models.DateField(help_text='Put date of first day of the week')
+    income = models.PositiveIntegerField()
