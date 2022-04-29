88a1c8d4a34cc54548961e2d635596a9c4781707
Hesam N <AnonCODER@tutanota.com>
2022-04-29 13:21:38 +0430

    Decrease and increase tests created

--- RevenueCalcSys/income/tests/test_income_models.py
+++ RevenueCalcSys/income/tests/test_income_models.py
@@ -21,3 +21,19 @@ class TestDailyIncome(TestCase):
         self.assertEqual(trip_3.income, daily_income_2.income)
         self.assertEqual(DailyIncome.objects.all().count(), 2)
         self.assertEqual(TripIncome.objects.all().count(), 3)
+    
+    def test_TripIncome_increases_by_IncreaseIncome(self):
+        '''Test TripIncome object increases by IncreaseIncome object'''
+        trip_1 = TripIncome.objects.create(courier=self.courier, income=100, trip_date=date(2022, 4, 29))
+        increase_income_1 = IncreaseIncome.objects.create(courier=self.courier, amount=12, descriptions='Test descriptions', date=date(2022, 4, 29))
+        daily_income = DailyIncome.objects.get(courier=self.courier, date=date(2022, 4, 29))
+        
+        self.assertEqual(daily_income.income, 112)
+    
+    def test_TripIncome_decreases_by_DecreaseIncome(self):
+        '''Test TripIncome object decrease by DecreaseIncome object'''
+        trip_1 = TripIncome.objects.create(courier=self.courier, income=100, trip_date=date(2022, 4, 29))
+        decrease_income_1 = DecreaseIncome.objects.create(courier=self.courier, amount=25, descriptions='Test descriptions', date=date(2022, 4, 29))
+        daily_income = DailyIncome.objects.get(courier=self.courier, date=date(2022, 4, 29))
+        
+        self.assertEqual(trip_1.income - decrease_income_1.amount, daily_income.income)
